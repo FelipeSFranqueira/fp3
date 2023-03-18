9ad4685051f495085eccb1d4958370bf9db19380
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2023-03-18 17:27:56 -0300

    feat: Implement Flat LaF

--- src/main/java/com/fp3/haras/view/container/Root.java
+++ src/main/java/com/fp3/haras/view/container/Root.java
@@ -1,8 +1,22 @@
 package com.fp3.haras.view.container;
 
 import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.formdev.flatlaf.FlatLightLaf;
 
 public class Root extends javax.swing.JFrame {
+    
+    static {
+        try {
+            UIManager.setLookAndFeel( new FlatLightLaf() );
+        } catch( UnsupportedLookAndFeelException e ) {
+            System.err.println( "Failed to initialize Flat LaF" );
+            e.printStackTrace();
+        }
+    }
+    
     private JPanel header;
     private JPanel sidenav;
     private JPanel mainContainer;
