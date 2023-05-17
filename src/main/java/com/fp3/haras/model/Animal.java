c69d07027fcbe68df030e1c720faa0128fbaa6ec
Felipe Sanches Franqueira <felipe04sfr@outlook.com>
2023-05-16 21:47:24 -0300

    feat: conect Animal class and Client class to JPA

--- src/main/java/com/fp3/haras/model/Animal.java
+++ src/main/java/com/fp3/haras/model/Animal.java
@@ -3,18 +3,35 @@ package com.fp3.haras.model;
 import com.fp3.haras.enums.Pelagem;
 import com.fp3.haras.enums.Sexo;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
+@Entity
 public class Animal {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private List<Cliente> proprietarios;
-    private String nome;
-    private Pelagem pelagem;
-    private Sexo sexo;
-    private boolean hasExameAie;
-    private boolean hasExameMormo;
+    
+    @ManyToMany
+    @JoinTable(
+        name = "OwnerXAnimal",
+        joinColumns = @JoinColumn(name = "owners_id"),
+        inverseJoinColumns = @JoinColumn(name = "animals_id")
+    )
+    private List<Cliente> owners;
+    
+    private String name;
+    private Pelagem coat;
+    private Sexo sex;
+    private boolean hasExamAie;
+    private boolean hasExamMormo;
     private boolean hasGta;
-    private List<Estadia> estadias;
     private boolean isDeleted;
 
     public Animal() {
@@ -28,52 +45,52 @@ public class Animal {
         this.id = id;
     }
 
-    public List<Cliente> getProprietarios() {
-        return proprietarios;
+    public List<Cliente> getOwners() {
+        return owners;
     }
 
-    public void setProprietarios(List<Cliente> proprietarios) {
-        this.proprietarios = proprietarios;
+    public void setOwners(List<Cliente> owners) {
+        this.owners = owners;
     }
 
-    public String getNome() {
-        return nome;
+    public String getName() {
+        return name;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public Pelagem getPelagem() {
-        return pelagem;
+    public Pelagem getCoat() {
+        return coat;
     }
 
-    public void setPelagem(Pelagem pelagem) {
-        this.pelagem = pelagem;
+    public void setCoat(Pelagem coat) {
+        this.coat = coat;
     }
 
-    public Sexo getSexo() {
-        return sexo;
+    public Sexo getSex() {
+        return sex;
     }
 
-    public void setSexo(Sexo sexo) {
-        this.sexo = sexo;
+    public void setSex(Sexo sex) {
+        this.sex = sex;
     }
 
     public boolean getHasExameAie() {
-        return hasExameAie;
+        return hasExamAie;
     }
 
-    public void setHasExameAie(boolean hasExameAie) {
-        this.hasExameAie = hasExameAie;
+    public void setHasExamAie(boolean hasExamAie) {
+        this.hasExamAie = hasExamAie;
     }
 
     public boolean getHasExameMormo() {
-        return hasExameMormo;
+        return hasExamMormo;
     }
 
-    public void setHasExameMormo(boolean hasExameMormo) {
-        this.hasExameMormo = hasExameMormo;
+    public void setHasExamMormo(boolean hasExamMormo) {
+        this.hasExamMormo = hasExamMormo;
     }
 
     public boolean getHasGta() {
@@ -82,14 +99,6 @@ public class Animal {
 
     public void setGta(boolean hasGta) {
         this.hasGta = hasGta;
-    }
-
-    public List<Estadia> getEstadias() {
-        return estadias;
-    }
-
-    public void setEstadias(List<Estadia> estadias) {
-        this.estadias = estadias;
     }
     
     public boolean getIsDeleted() {
